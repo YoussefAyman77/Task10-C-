0076e8f5a828376b03312edb49679c7696b2e332
YoussefAyman77 <youssefstemer@gmail.com>
2026-02-24 16:43:09 +0200

    Add project files.

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sab3!");
+        }
+    }
+}
